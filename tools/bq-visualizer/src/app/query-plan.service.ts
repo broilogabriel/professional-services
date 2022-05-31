091aa9d1319e4a8c9b5a87bbdeb6cb8901f55a6f
Gabriel Broilo Luz <gabriel.broiloluz@rvu.co.uk>
2022-05-31 11:43:13 +0100

    feat: add docker container for bq-visualizer

--- tools/bq-visualizer/src/app/query-plan.service.ts
+++ tools/bq-visualizer/src/app/query-plan.service.ts
@@ -13,20 +13,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {Injectable} from '@angular/core';
-import {Observable, of} from 'rxjs';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
-import {BqQueryPlan} from './bq_query_plan';
-import {LogService} from './log.service';
+import { BqQueryPlan } from './bq_query_plan';
+import { LogService } from './log.service';
 
 
 /**
  * Manages the instance of BQJob that needs to be rendered by components.
  * Also provides an upload service of BQJob json files via a file reader.
  */
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class QueryPlanService {
-  constructor(private logSvc: LogService) {}
+  constructor(private logSvc: LogService) { }
 
   /** Upload a file from local system. */
   upload(file: File): Promise<BqQueryPlan> {
@@ -37,11 +37,13 @@ export class QueryPlanService {
       reader.onloadend = e => {
         try {
           const plan =
-              new BqQueryPlan(JSON.parse(reader.result as string), this.logSvc);
+            new BqQueryPlan(JSON.parse(reader.result as string), this.logSvc);
           this.logSvc.debug('QueryPlan loaded from uploaded file');
           resolve(plan);
         } catch (e) {
-          this.logSvc.error(e);
+          if (e instanceof Error) {
+            this.logSvc.error(e.message);
+          }
           alert(e);
           reject(e);
         }
